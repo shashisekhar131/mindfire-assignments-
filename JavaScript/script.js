5bd733e1a4ca27dec480829e26d8400662dd8015
shashi <shashi184131@gmail.com>
2024-01-18 16:32:09 +0530

    showing inputs in consistent manner and updated some CSS

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -53,16 +53,16 @@ var popUpElement = $("#pop-up");
 // show pop-up
 function showPopUp(message){
 
-  // Create close button and add functionality
-  var closeButton = $('<button>').html('OK').on('click', function () {
-     popUpElement.hide();
- });
+    // Create close button and add functionality
+    var closeButton = $('<button>').html('OK').on('click', function () {
+        popUpElement.hide();
+    });
 
- // Attach message and close button
- popUpElement.html(`<div>${message}</div>`).append(closeButton);
+    // Attach message and close button
+    popUpElement.html(`<div>${message}</div>`).append(closeButton);
 
- // Display the pop-up 
- popUpElement.show();
+    // Display the pop-up 
+    popUpElement.show();
 
 }
 
@@ -70,42 +70,42 @@ function showPopUp(message){
 var userDetails = [];
 
  function getDetails(inputElements){
- 
- // get the answer for inputs of user
- for(let i=0;i<inputElements.length;i++){
-     if(inputElements[i].type != "radio")  userDetails.push({[inputElements[i].name]:inputElements[i].value});
-    }
+    
+    // get the answer for inputs of user
+    for(let i=0;i<inputElements.length;i++){
+        if(inputElements[i].type != "radio")  userDetails.push({[inputElements[i].name]:inputElements[i].value});
+        }
 
-     // get the answer for the radio option gender and marital staus      
-    userDetails.push({ gender: $('input[name="gender"]:checked').val() });
-    userDetails.push({ "marital-status": $('input[name="marital-status"]:checked').val() });
+        // get the answer for the radio option gender and marital staus      
+        userDetails.push({ gender: $('input[name="gender"]:checked').val() });
+        userDetails.push({ "marital-status": $('input[name="marital-status"]:checked').val() });
 
-     // get the answer from hobbies means if hobbies checked is others then show prompt else directly push value
-     $('input[name="hobbies"]:checked').each(function() {
-         userDetails.push({ "hobbies": this.value === "others" ? prompt("Enter the hobbies") : this.value });
-     });
+        // get the answer from hobbies means if hobbies checked is others then show prompt else directly push value
+        $('input[name="hobbies"]:checked').each(function() {
+            userDetails.push({ "hobbies": this.value === "others" ? prompt("Enter the hobbies") : this.value });
+        });
 
-     // form validated and got details now show the details 
-     showDetails();
+        // form validated and got details now show the details 
+        showDetails();
 }
 
 
 // show the details 
 function showDetails(){
 
- // using string builder instead of looping and concatenating the strings
- const detailsStringBuilder = ['<h2>Details you submitted</h2>'];
+    // using string builder instead of looping and concatenating the strings
+    const detailsStringBuilder = ['<h2>Details you submitted</h2>'];
 
- userDetails.forEach(userObject => {
-     for (const [property, value] of Object.entries(userObject)) {
-         detailsStringBuilder.push(`${property} : ${value}<br>`);
-     }
- });
+    userDetails.forEach(userObject => {
+        for (const [property, value] of Object.entries(userObject)) {
+            detailsStringBuilder.push(`${property} : ${value}<br>`);
+        }
+    });
 
- const detailsString = detailsStringBuilder.join('');
- showPopUp(detailsString);
+    const detailsString = detailsStringBuilder.join('');
+    showPopUp(detailsString);
 
- localStorage.setItem("storedDetailsString",JSON.stringify(detailsString));
+    localStorage.setItem("storedDetailsString",JSON.stringify(detailsString));
 }
 
 
@@ -132,43 +132,45 @@ if (popUpElement.is(":visible")) {
 var textInputs = $('input');
 
 textInputs.on('input', function() {
- var currentInput = $(this);
-if(currentInput.is('[data-allowed="text"]')){
 
- //(currentInput.val() !== "") condtion is  when there is no input don't show errors
+    var currentInput = $(this);
+    if(currentInput.is('[data-allowed="text"]')){
 
- if (currentInput.val() !== "" && !/^[a-zA-Z]+$/.test(currentInput.val())) {
-     showErrorTip("only text allowed",currentInput);
- } else removeErrorTip(currentInput);
-    
-}
+    //(currentInput.val() !== "") condtion is  when there is no input don't show errors
 
-if(currentInput.is('[type="password"]')){
+    if (currentInput.val() !== "" && !/^[a-zA-Z]+$/.test(currentInput.val())) {
+        showErrorTip("only text allowed",currentInput);
+    } else removeErrorTip(currentInput);
+        
+    }
 
-if(currentInput.val() !== "" && currentInput.val().length < 8) showErrorTip("minimum 8 characters",currentInput);
-else  removeErrorTip(currentInput);
+    if(currentInput.is('[type="password"]')){
 
-}
+    if(currentInput.val() !== "" && currentInput.val().length < 8) showErrorTip("minimum 8 characters",currentInput);
+    else  removeErrorTip(currentInput);
 
-if(currentInput.is('[type="tel"]')){
+    }
 
- if(currentInput.val() !== "" && currentInput.val().length != 10) showErrorTip("it should be 10 characters",currentInput);
- else  removeErrorTip(currentInput);
+    if(currentInput.is('[type="tel"]')){
 
-}
+    if(currentInput.val() !== "" && currentInput.val().length != 10) showErrorTip("min 10 characters",currentInput);
+    else  removeErrorTip(currentInput);
 
-if(currentInput.is('[name="email"]')){
+    }
 
- if(currentInput.val() !== "" && !(/^[a-zA-Z0-9._-]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/).test(currentInput.val())) showErrorTip("enter valid email",currentInput);
- else  removeErrorTip(currentInput);
+    if(currentInput.is('[name="email"]')){
 
-}
+    if(currentInput.val() !== "" && !(/^[a-zA-Z0-9._-]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/).test(currentInput.val())) showErrorTip("enter valid email",currentInput);
+    else  removeErrorTip(currentInput);
 
-if(currentInput.is('[data-allowed="marks"]')){
- if(currentInput.val() !== "" && !(/^([0-9]{1,2})$/).test(currentInput.val())) showErrorTip("enter valid percentage",currentInput);
- else  removeErrorTip(currentInput);
+    }
 
-}
+    if(currentInput.is('[data-allowed="marks"]')){
+
+    if(currentInput.val() !== "" && !(/^([0-9]{1,2})$/).test(currentInput.val())) showErrorTip("enter valid percentage",currentInput);
+    else  removeErrorTip(currentInput);
+
+    }
 
  
 });
@@ -176,12 +178,18 @@ if(currentInput.is('[data-allowed="marks"]')){
 
 
 function showErrorTip(message,currentInput){
- currentInput.next().html(message);
- currentInput.css('borderColor', 'red');
+    currentInput.next().html(message);
+    currentInput.css({
+        borderColor: 'red',
+        boxShadow: '0 0 8px rgba(255, 0, 0, 0.5)'
+    });
+    
 }
 function removeErrorTip(currentInput){
- currentInput.next().html("");
- currentInput.css('borderColor', 'rgba(120,120,120,0.5)');
-
+    currentInput.next().html("");
+    currentInput.css({
+        borderColor: 'rgba(120,120,120,0.5)',
+        boxShadow: '0 0 0 white'
+    });
 }
 
