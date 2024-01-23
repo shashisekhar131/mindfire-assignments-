63dca4e3cd3827b99bfafdbe528ef455fa92bddd
shashi <shashi184131@gmail.com>
2024-01-23 13:21:20 +0530

    read/write into a file

---
+++ ConsoleApp1(assingment)/ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter some text that you want to write to file");
+
+            // for the first time             
+            File.WriteAllText("randomFile.txt",Console.ReadLine());
+            Console.WriteLine("enter the option add / read / stop");
+
+            // do go away from console until user enters stop
+            string userInput = Console.ReadLine().ToLower();
+
+            while (userInput != "stop") {
+
+                if(userInput == "add") {
+
+                    try
+                    {
+                        // Append text to the existing file
+                        using (StreamWriter sw = File.AppendText("randomFile.txt"))
+                        {
+                            Console.WriteLine("enter data that you want to add:");
+                            sw.WriteLine(Console.ReadLine());
+                        }
+
+                        Console.WriteLine("Text appended successfully!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"An error occurred: {ex.Message}");
+                    }
+                
+                }else if(userInput == "read")
+                {
+
+                    try
+                    {
+                        // Read all text from the file
+                        string fileContent = File.ReadAllText("randomFile.txt");
+
+                        // showing the content on console
+                        Console.WriteLine("File Content:");
+                        Console.WriteLine(fileContent);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"An error occurred: {ex.Message}");
+                    }
+
+                }
+
+              Console.WriteLine("enter the option add / read / stop");
+              userInput = Console.ReadLine().ToLower();
+                
+               
+
+            }
+
+
+        }
+    }
+}
