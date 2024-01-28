703fe2c87cc220dee935de3e110f4aca5e790ad0
shashi <shashi184131@gmail.com>
2024-01-28 22:01:22 +0530

    dataTableToList, ListToDataTable extension methods

---
+++ ExtensionMethods/ExtensionMethods/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionMethods
+{
+    public static class DataTableExtensions
+    {
+        // Extension method to convert DataTable to a list of objects
+
+        public static List<T> ToList<T>(this DataTable dataTable)
+        {
+            List<T> result = new List<T>();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                T item = Activator.CreateInstance<T>();
+
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    var property = typeof(T).GetProperty(column.ColumnName);
+
+                    if (property != null && row[column] != DBNull.Value)
+                    {
+                        property.SetValue(item, row[column]);
+                    }
+                }
+
+                result.Add(item);
+            }
+
+            return result;
+        }
+
+
+        // Extension method to convert a list of objects to DataTable
+        public static DataTable ToDataTable<T>(this List<T> list)
+        {
+            DataTable dataTable = new DataTable();
+
+
+                // add columns to DataTable
+                foreach (var prop in list[0].GetType().GetProperties())
+                {
+                    dataTable.Columns.Add(prop.Name, prop.PropertyType);
+                }
+
+                // Add rows to DataTable 
+                foreach (var item in list)
+                {
+                    DataRow row = dataTable.NewRow();
+
+                    foreach (var prop in item.GetType().GetProperties())
+                    {
+                        row[prop.Name] = prop.GetValue(item)??DBNull.Value ;
+                    }
+
+                    dataTable.Rows.Add(row);
+                }
+            
+
+            return dataTable;
+        }
+    }
+
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConversionMethods;
+
+namespace ExtensionMethods
+{
+
+    public class Animal
+    {
+        public string Name { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            List<Animal> li = new List<Animal>() { new Animal { Name = "Lion" },
+            new Animal { Name = "Dear" },
+            new Animal { Name = "Wolf" },
+            new Animal { Name = "Tiger" }};
+
+            Console.Write("List: ");
+           
+            foreach (var item in li)
+            {
+                Console.Write(item.Name);
+            }
+           
+            Console.WriteLine("\n \nconverted above list to data table");
+            Console.WriteLine();
+            Console.WriteLine("Data Table:");
+            DataTable d =  li.ToDataTable();
+
+            foreach (DataRow row in d.Rows)
+            {
+                foreach (DataColumn col in d.Columns)
+                {
+                    Console.Write($"{col.ColumnName}: {row[col]}   ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("converted above dataTable to list");
+            Console.WriteLine();
+
+            li =  d.ToList<Animal>();
+
+            foreach (var item in li)
+            {
+                Console.Write(item.Name);
+            }
+           
+            Console.ReadLine();
+
+        }
+    }
+}
