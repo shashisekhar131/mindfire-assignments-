59f278caf057b310d4e8cfe01521a5967833428a
shashi <shashi184131@gmail.com>
2024-01-29 15:46:47 +0530

    updated functions in manyTomany Tables Db-first

--- DBFirstManyTables/DBFirstManyTables/Program.cs
+++ DBFirstManyTables/DBFirstManyTables/Program.cs
@@ -15,8 +15,8 @@ namespace DBFirstManyTables
             do
             {
                 Console.WriteLine(" 1. ReadStudentTable \n 2. insert student Data \n 3. delete student row \n" +
-                    "4. ReadCourseTable \n 5. insert Course Data \n 6. delete Course row \n"+
-                    "7. ReadCourseTable \n 8. insert Course Data \n 9. delete Course row \n 10.stop"
+                    "4. ReadCourseTable \n 5. insert Course Data \n 6. delete Course row \n" +
+                    "7. ReadCourseTable \n 8. insert Course Data \n 9. delete Course row \n 10.assign student to course \n 11.assgin course to grade \n 12.remove course from grade \n 13.remove student from grade \n 14.all stuents in grades \n 15.courses in grades \n 16.stop"
                     );
 
                 selected = Convert.ToInt32(Console.ReadLine());
@@ -30,7 +30,7 @@ namespace DBFirstManyTables
                         InsertStudent();
                         break;
                     case 3:
-                       DeleteStudentRow();
+                        DeleteStudentRow();
                         break;
                     case 4:
                         ReadCourseTable();
@@ -50,9 +50,28 @@ namespace DBFirstManyTables
                     case 9:
                         DeleteGradeRow();
                         break;
+                    case 10:
+                        AssignStudentToGrade();
+                        break;
+                    case 11:
+                        AssignCourseToGrade();
+                        break;
+                    case 12:
+                        RemoveCourseFromGrade();
+                        break;
+                    case 13:
+                        RemoveStudentFromGrade();
+                        break;
+                    case 14:
+                        DisplayAllStudentInGrades();
+                        break;
+                    case 15:
+                        DisplayAllCourseGrades();
+                        break;
+
                 }
 
-            } while (selected != 10);
+            } while (selected != 16);
 
         }
 
@@ -69,7 +88,8 @@ namespace DBFirstManyTables
                 string newName = Console.ReadLine();
 
                 // create Student object
-                var newStudent = new Student{
+                var newStudent = new Student
+                {
                     Name = newName,
                 };
 
@@ -101,7 +121,7 @@ namespace DBFirstManyTables
 
         public static void DeleteStudentRow()
         {
-           
+
             Console.WriteLine("from the table select the id");
 
 
@@ -110,7 +130,8 @@ namespace DBFirstManyTables
                 Console.WriteLine("Enter the student ID to delete:");
 
                 var studentToDelete = context.Students.Find(Convert.ToInt32(Console.ReadLine()));
-                try {
+                try
+                {
                     if (studentToDelete != null)
                     {
                         context.Students.Remove(studentToDelete);
@@ -160,7 +181,7 @@ namespace DBFirstManyTables
                 // create Student object
                 var newCourse = new Course
                 {
-                   CourseName = newName,
+                    CourseName = newName,
                 };
 
                 // pass it to context API 
@@ -331,6 +352,216 @@ namespace DBFirstManyTables
 
         }
 
+
+
+        // we are creating using  exiting database and there is no third column in rlationship table so STudentGrades will not be in EF 
+        public static void AssignStudentToGrade()
+        {
+            Console.WriteLine("Enter Student ID:");
+            int studentId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Grade ID:");
+            int gradeId = int.Parse(Console.ReadLine());
+
+            try
+            {
+                using (var context = new XYZSchoolDBEntities())
+                {
+                    Student student = context.Students.Find(studentId);
+                    Grade grade = context.Grades.Find(gradeId);
+
+                    if (student != null && grade != null)
+                    {
+                        // Assign the student to the grade directly
+                        student.Grades.Add(grade);
+
+                        context.SaveChanges();
+
+                        Console.WriteLine("Student assigned to Grade successfully.");
+                        LoggerClass.AddData("assgined student to grade ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Student or Grade not found.");
+                        LoggerClass.AddData("error in assiging student to grade");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+        public static void AssignCourseToGrade()
+        {
+            Console.WriteLine("Enter Course ID:");
+            int courseId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Grade ID:");
+            int gradeId = int.Parse(Console.ReadLine());
+
+            try
+            {
+                using (var context = new XYZSchoolDBEntities())
+                {
+                    Course course = context.Courses.Find(courseId);
+                    Grade grade = context.Grades.Find(gradeId);
+
+                    if (course != null && grade != null)
+                    {
+                        // Assign the course to the grade directly
+                        course.Grades.Add(grade);
+
+                        context.SaveChanges();
+
+                        Console.WriteLine("Course assigned to Grade successfully.");
+                        LoggerClass.AddData("assigned course to grade");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Course or Grade not found.");
+                        LoggerClass.AddData("error in assiging course to grade");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+        public static void RemoveCourseFromGrade()
+        {
+            Console.WriteLine("Enter Course ID:");
+            int courseId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Grade ID:");
+            int gradeId = int.Parse(Console.ReadLine());
+
+            try
+            {
+                using (var context = new XYZSchoolDBEntities())
+                {
+                    Course course = context.Courses.Find(courseId);
+                    Grade grade = context.Grades.Find(gradeId);
+
+                    if (course != null && grade != null)
+                    {
+                        // Remove the course from the grade
+                        course.Grades.Remove(grade);
+
+                        context.SaveChanges();
+
+                        Console.WriteLine("Course removed from Grade successfully.");
+                        LoggerClass.AddData("removed course from grade");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Course or Grade not found.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+        public static void RemoveStudentFromGrade()
+        {
+            Console.WriteLine("Enter Student ID:");
+            int studentId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Grade ID:");
+            int gradeId = int.Parse(Console.ReadLine());
+
+            try
+            {
+                using (var context = new XYZSchoolDBEntities())
+                {
+                    Student student = context.Students.Find(studentId);
+                    Grade grade = context.Grades.Find(gradeId);
+
+                    if (student != null && grade != null)
+                    {
+                        // Remove the student from the grade
+                        student.Grades.Remove(grade);
+
+                        context.SaveChanges();
+
+
+                        Console.WriteLine("Student removed from Grade successfully.");
+                        LoggerClass.AddData("removed student from grade");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Student or Grade not found.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+        public static void DisplayAllStudentInGrades()
+        {
+            try
+            {
+                using (var context = new XYZSchoolDBEntities())
+                {
+                    var students = context.Students.Include("Grades");
+
+                    Console.WriteLine("All StudentsInGrades:");
+                    foreach (var student in students)
+                    {
+                        foreach (var grade in student.Grades)
+                        {
+                            Console.WriteLine($"Student ID: {student.StudentID}, Grade ID: {grade.GradeID}");
+                        }
+                    }
+                    LoggerClass.AddData("displayed all students in grades ");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+
+        public static void DisplayAllCourseGrades()
+        {
+            try
+            {
+                using (var context = new XYZSchoolDBEntities())
+                {
+                    var courses = context.Courses.Include("Grades");
+
+                    Console.WriteLine("All CourseGrades:");
+                    foreach (var course in courses)
+                    {
+                        foreach (var grade in course.Grades)
+                        {
+                            Console.WriteLine($"Course ID: {course.CourseID}, Grade ID: {grade.GradeID}");
+                        }
+                    }
+
+                    LoggerClass.AddData("displayed all courses in grades");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
 
 
     }
