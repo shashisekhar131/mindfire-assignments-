d98991a0438792ae0a5dc91e93f99a44f700da0e
shashi <shashi184131@gmail.com>
2024-03-02 17:52:49 +0530

    added LoginV2controller file

---
+++ DemoUserManagementMVC/DemoUserManagementMVC/Controllers/LoginV2Controller.cs
@@ -0,0 +1,46 @@
+﻿using DemoUserManagement.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyService = DemoUserManagement.Business.Service;
+
+namespace DemoUserManagementMVC.Controllers
+{
+    public class LoginV2Controller : Controller
+    {
+        static MyService service = new MyService();
+
+        // GET: LoginV2
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CheckIfUserExists(string UserEmail, string UserPassword)
+        {  // UserEmail, UserPassword parameters get values using input name in html 
+
+            Dictionary<string, int> User = service.CheckIfUserExists(UserEmail, UserPassword);
+
+            if (User["IsUserExists"] == 1)
+            {
+                // set the session data 
+                SessionClass sessionData = new SessionClass
+                {
+                    UserID = User["UserID"],
+                    UserRole = User["RoleID"]
+                };
+
+                Session["UserSession"] = sessionData;
+
+            }
+
+            return Json(User, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+
+    }
+}
---
+++ DemoUserManagementMVC/DemoUserManagementMVC/Scripts/LoginV2.js
@@ -0,0 +1,45 @@
+﻿
+function AuthenticateUser(UserEmail, UserPassword) {
+    $.ajax({
+        url: '/LoginV2/CheckIfUserExists',
+        type: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify({ UserEmail, UserPassword }), // Send the countryName parameter
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            var User = data;
+            if (User["IsUserExists"]) {
+                console.log("your a valid user");
+                document.getElementById("tip").innerHTML = "your a valid user";
+                window.location.href = "/UserRegistration/Index/"+ User["UserID"];
+            } else {
+                console.log("please first sign up");
+                document.getElementById("tip").innerHTML = "please create an account";
+            }
+
+
+        },
+        error: function (xhr, status, error) {
+            console.log('Error loading states: ', error);
+            console.log(xhr.responseText);
+        }
+    });
+}
+
+
+$(document).ready(function () {
+
+    $('#SubmitBtn').on('click', function (event) {
+        event.preventDefault(); // Prevent the default form submission
+        console.log("entered" + $('#UserEmail').val() + $('#UserPassword').val());
+        AuthenticateUser($('#UserEmail').val(), $('#UserPassword').val());
+
+    });
+
+    $('#RegisterBtn').on('click', function (event) {
+        event.preventDefault();
+        window.location.href = "UserDetails.aspx";
+    })
+
+});
